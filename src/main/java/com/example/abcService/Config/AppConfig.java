ddf2a664312491fa9e49465f7dd5f86a6810e1ef
bloodeclipsexd <zadoinov99@gmail.com>
2021-12-12 18:45:35 +0200

    asda

--- src/main/java/com/example/abcService/Config/AppConfig.java
+++ src/main/java/com/example/abcService/Config/AppConfig.java
@@ -4,11 +4,20 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetSocketAddress;
+import java.security.NoSuchAlgorithmException;
+
 @Configuration
 public class AppConfig{
 
     @Bean
-    public CqlSession session() {
-        return CqlSession.builder().withKeyspace("pad").withLocalDatacenter("datacenter1").build();
+    public CqlSession session() throws NoSuchAlgorithmException {
+
+        return CqlSession.builder()
+                .withKeyspace("pad")
+                .addContactPoint(new InetSocketAddress("pad-cassandra.cassandra.cosmos.azure.com", 10350))
+                .withCredentials("pad-cassandra", "4xav9FZWNHuF87ELo6TIkh7UuQIzU5lAqi5jIAzTBC5VPEuqN7cVMqjN4XHkrQ1jcEb3l9K0uPBjtccRyM5Gag==")
+                .build();
     }
+
 }
